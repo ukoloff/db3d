511927c08604ff39f7b4df7e6a1505923d3339f7
Stas Ukolov <ukoloff@gmail.com>
2016-10-03 19:22:53 +0500

    Start show page

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -4,4 +4,20 @@ class ToolsController < ApplicationController
       render json: Tool.all
     end
   end
+
+  def show
+    item or return
+  end
+
+  private
+
+  def item
+    tool = Tool.find_by_id params[:id]
+    if tool
+      @tool = tool
+    else
+      redirect_to tools_url
+      nil
+    end
+  end
 end
