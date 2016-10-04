ff44defca6d0017b8c28f35ed46e7c3700e8e6cd
Stas Ukolov <ukoloff@gmail.com>
2016-10-04 15:47:19 +0500

    Optimize fetch

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -8,4 +8,9 @@ class Tool < ActiveRecord::Base
   def date2str
     date.localtime.strftime '%d.%m.%Y' rescue nil
   end
+
+  def foto_name
+    f = Foto.where(tool_id: id).first
+    f.name if f
+  end
 end
