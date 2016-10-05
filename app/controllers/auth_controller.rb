fb53e62915f053c2cce470459bea6d064f975e58
Stas Ukolov <ukoloff@gmail.com>
2016-10-05 12:07:47 +0500

    Add Twitter

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,6 @@
 class AuthController < ActionController::Base
   def callback
     auth_hash = request.env['omniauth.auth']
-    render plain: YAML.dump(auth_hash)
+    render plain: YAML.dump(auth_hash.as_json)
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   YAML.load_file(Rails.root + 'config/omniauth.yml').each do |k, v|
-    provider k, *ENV["OA_#{k}"].split(':'), v
+    provider k, *ENV["OA_#{k}"].split(':'), v || {}
   end
 end
