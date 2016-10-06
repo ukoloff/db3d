033e88c23bb11ec76bcefefaee6aa2ac3d906b8d
Stas Ukolov <ukoloff@gmail.com>
2016-10-06 19:43:34 +0500

    Last oauth for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   acts_as_paranoid
 
   has_many :oauths, -> { order updated_at: :desc}
+  has_one :oauth, -> { order updated_at: :desc}
 
   def name
-    oauths.first.info['info']['name'] rescue nil
+    oauth.info['info']['name'] rescue nil
   end
 end
