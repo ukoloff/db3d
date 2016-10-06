6451836e8d9cfc4bf96964fa67f75b3de6290674
Stas Ukolov <ukoloff@gmail.com>
2016-10-06 19:54:25 +0500

    Redo user creation

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,11 +3,8 @@ class AuthController < ApplicationController
     z = request.env['omniauth.auth'].as_json
     oauth = Oauth.where(z.slice *%w(provider uid)).first_or_initialize
     oauth.info = z
-    session[:uid] = if oauth.user
-       oauth.user.id
-    else
-      (oauth.user = User.find_by_id(session[:uid]) || User.create).id
-    end
+    oauth.user ||= User.find_by_id(session[:uid]) || User.create
+    session[:uid] = oauth.user.id
     oauth.save
     redirect_to auth_path
   end
