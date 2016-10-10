328b4bec1bbbb670c90bbeacffcc029c7d3861b2
Stas Ukolov <ukoloff@gmail.com>
2016-10-10 21:39:15 +0500

    Avoid orphaned oauths

--- app/models/oauth.rb
+++ app/models/oauth.rb
@@ -15,4 +15,10 @@ class Oauth < ActiveRecord::Base
     super data
     info_will_change!
   end
+
+  # Все авторизации, для которых пользователь не удалён
+  scope :unorphaned, -> do
+    # Приходится писать в условии users: вместо user: (???)
+    joins(:user).where.not users: {id: nil}
+  end
 end
