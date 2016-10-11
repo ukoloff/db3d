d7a215e4da428f089b6b62dbaa8847a16b263f19
Stas Ukolov <ukoloff@gmail.com>
2016-10-11 14:33:06 +0500

    Default sort order

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -1,7 +1,7 @@
 class ToolsController < ApplicationController
   def index
     if params.key? 'js'
-      render json: Tool.all.as_json(only: %w(id name note), methods: %w(date2str))
+      render json: Tool.order(:id).as_json(only: %w(id name note), methods: %w(date2str))
     end
   end
 
