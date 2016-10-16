f5431934337478fd3558542e389b783844f4dbb5
Stas Ukolov <ukoloff@gmail.com>
2016-10-16 19:52:09 +0500

    Add Tool#tagnames

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -6,7 +6,7 @@ class Tool < ActiveRecord::Base
   has_one :foto
   belongs_to :creator, class_name: 'User'
   has_many :t2ts
-  has_many :tags, ->{ order :name }, through: :t2ts
+  has_many :tags, through: :t2ts
 
   def date2str
     date.localtime.strftime '%d.%m.%Y' rescue nil
@@ -15,5 +15,13 @@ class Tool < ActiveRecord::Base
   def foto_name
     f = Foto.where(tool_id: id).select(:name).first
     f.name if f
+  end
+
+  def tagnames
+    tags.order(1).pluck :name
+  end
+
+  def tagnames= names
+    # TODO: Implement!
   end
 end
